9900eb277f5b15f00728f486138713477cd97441
AN773 <29415627+AN-773@users.noreply.github.com>
2023-12-13 05:01:44 +0300

    connect to db

--- WebProje/Program.cs
+++ WebProje/Program.cs
@@ -1,7 +1,31 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebProje.Data;
+using Microsoft.AspNetCore.Identity;
+using WebProje.Models;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+using System.Reflection;
+using Microsoft.Extensions.Options;
+using WebProje.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+builder.Services.AddDbContext<ApplicationContext>(
+    dbContextOptions => dbContextOptions
+        .UseMySql(builder.Configuration.GetConnectionString("default"),
+            ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("default")))
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors()
+);
 
 var app = builder.Build();
 
